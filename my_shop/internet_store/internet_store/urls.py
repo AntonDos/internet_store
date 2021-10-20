aead4f293fd03d5966f98da0a9b15db341a40bab
AntonDos <adosmenesk@pm.me>
2021-10-20 15:40:06 +0300

    refactor

--- my_shop/internet_store/internet_store/urls.py
+++ my_shop/internet_store/internet_store/urls.py
@@ -1,4 +1,4 @@
-"""internet_store URL Configuration
+"""shop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', include('mainapp.urls')),
+    path('product-specs/', include('specs.urls'))
 ]
 
 if settings.DEBUG:
